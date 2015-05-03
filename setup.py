f44c39453d25fd80230ab96744f3d7ea844642f7
David P. Kendal <dpkendal@dpk.org.uk>
2015-05-03 19:23:42 +0200

    setup.py fixes

--- setup.py
+++ setup.py
@@ -4,19 +4,21 @@ from distutils.core import setup
 
 setup(
     name='ikwi',
-    version='0.1',
+    version='0.1.4',
     description='A personal wikiware.',
     author='David P. Kendal',
     author_email='pypi@dpk.io',
     url='https://github.com/dpk/ikwi',
     packages=['ikwi'],
     package_dir={'ikwi': '.'},
+    package_data={'ikwi': ['js/*.js']},
     scripts=['ikwi'],
-    requires=[
+    install_requires=[
         'bcrypt>=1.1.1',
         'html5lib==0.999',
         'Jinja2>=2.7.3',
         'lxml>=3.4.3',
+        'pygit2==0.22.0',
         'pypandoc==0.9.7',
         'PyYAML==3.11',
         'Werkzeug>=0.10.4',
